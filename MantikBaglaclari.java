7684e47b0b95ac42414b3a9d4ba570f572f8c6b5
Kubilay Kahil <kahil_kubilay@hotmail.com>
2019-03-25 17:16:20 +0300

    Update MantikBaglaclari.java
    Similar method

--- MantikBaglaclari.java
+++ MantikBaglaclari.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class MantikBaglaclari {
 
     public static void main(String[] args) {
-        Scanner a = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         int sayi1, sayi2, sayi3;
         System.out.print("İlk Sayıyı Giriniz: ");
-        sayi1 = a.nextInt();
+        sayi1 = input.nextInt();
         System.out.print("2. Sayıyı Giriniz: ");
-        sayi2 = a.nextInt();
+        sayi2 = input.nextInt();
         System.out.print("Son Sayıyı Giriniz: ");
-        sayi3 = a.nextInt();
+        sayi3 = input.nextInt();
 
         //İlk Girilen Sayı 2. Sayı ve 3. Sayı değerinin arasında mı?
         if (sayi1 > sayi2 && sayi1 < sayi3) {
@@ -38,5 +38,31 @@ public class MantikBaglaclari {
         } else {
             System.out.println("Girilen sayılar birbirlerine eşit değildir.");
         }
+        
+        input.close();
+        
+        //diğer bir yöntem
+        /*
+        //son durum mümkünse, diğerlerini kontrol etmek gerekmiyor.
+		if( a == b && b == c) {
+			System.out.print("a, b ile c arasında bir sayı değildir\n"
+					+ "a, b veya c'den daha büyük değildir.\n"
+					+ "Üç sayı birbirine eşittir.");
+		}else {
+			//birinci durum
+			if(a > b && a < c || a > c && a<b ) System.out.println("a, b ile c arasında bir sayıdır");
+			else System.out.println("a, b ile c arasında bir sayı değildir.");
+			
+			//ikinci durum
+			if(a == b && a < c) System.out.println("a, b'ye eşit ve c'de küçüktür.");
+			else System.out.println("a hem b'ye eşit hem de c'den küçük değildir.");
+			
+			//üçüncü durum
+			if(a > b) System.out.println("a b'den büyüktür.");
+			
+			if(a > c) System.out.println("a c'den büyüktür.");
+			
+		}
+        */
     }
 }
