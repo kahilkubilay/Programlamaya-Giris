7aeabfe8a24d6b4926eea272f76ab1c3cee12dc7
Kubilay Kahil <kahil_kubilay@hotmail.com>
2019-03-25 17:58:22 +0300

    Create  1den10aDongu.java

---
+++ 1den10aDongu.java
@@ -0,0 +1,16 @@
+package githubBilgisayarKavramlari;
+
+public class P9_github {
+
+	public static void main(String[] args) {
+		// TODO Q9::1'den 10'a kadar olan ardışık sayıları
+		// ekrana bastıran kodu yazınız.
+		
+		for(int i = 0 ; i<10; i++) {
+			System.out.println((i+1));
+		}
+		
+		System.exit(0);
+
+	}
+}
